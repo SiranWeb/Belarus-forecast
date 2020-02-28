3fedcd6279deeb696505aaa519f080e69ff3af21
SiranWeb <kirillg.web@gmail.com>
2020-02-28 20:51:05 +0300

    getting main weather parameters

--- src/elements.js
+++ src/elements.js
@@ -1,3 +1,7 @@
 export const cityName = document.querySelector('.city-name');
 export const citySelect = document.querySelector('#cities');
 export const imgWeather = document.querySelector('.weather-img');
+export const weatherDescrp = document.querySelector('.weather-descrp');
+export const cityTemp = document.querySelector('.city-temperature');
+export const windArrow = document.querySelector('.wind-arrow');
+export const windSpeed = document.querySelector('.wind-speed');
--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import {fetchWeather, fetchForecast} from './weather.js';
-import {citySelect, cityName, imgWeather} from './elements.js';
+import {citySelect, cityName, imgWeather, weatherDescrp, cityTemp, windArrow, windSpeed} from './elements.js';
 
 function getWeather(city) {
-    console.log(city);
     fetchWeather(city)
     .then(data => displayWeather(data));
 };
 
 function displayWeather(data) {
     console.log(data);
-    cityName.innerHTML = data.name;
-}
+    // Grodno name fix
+    cityName.innerHTML = data.name == 'Hrodna' ? 'Grodno' : data.name;
+    imgWeather.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    weatherDescrp.innerHTML = data.weather[0].description;
+    cityTemp.innerHTML = Math.floor(data.main.temp - 273.15) + '°С';
+    windArrow.style.transform = `rotate(${data.wind.deg + 90}deg)`;
+    windSpeed.innerHTML = `${data.wind.speed} m/s`;
+};
 
 getWeather();
 
 citySelect.addEventListener('change', () => getWeather(citySelect.options[citySelect.selectedIndex].value));
-
-console.log(citySelect.options[citySelect.selectedIndex].value);
