2be3c55edd0df989bba21b5d50e54dd0e5fea94c
SiranWeb <kirillg.web@gmail.com>
2020-03-22 11:43:29 +0300

    prepare for GitHub

---
+++ README.md
@@ -0,0 +1,23 @@
+###About
+Weather forecast of Belarus.
+
+- Current info about city
+- 5 days forecast
+- Graph of current temperatures
+
+
+------------
+
+
+###Used
+
+- OpenWeatherMap API
+- Google Charts
+
+
+------------
+
+
+
+###Try it
+Link: [siranweb.github.io/Belarus-forecast](https://siranweb.github.io/Belarus-forecast/ "siranweb.github.io/Belarus-forecast")
--- src/weather.js
+++ src/weather.js
@@ -2,12 +2,12 @@ const API_KEY = '27ffc02e273de1d4f074adfb4a68c75e';
 
 export async function fetchWeather(city) {
     city == undefined ? city = 'minsk' : city = city;
-    const rawData = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`)
+    const rawData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`)
     return await rawData.json();
 };
 
 export async function fetchForecast(city) {
     city == undefined ? city = 'minsk' : city = city;
-    const rawData = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${API_KEY}`);
+    const rawData = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${API_KEY}`);
     return await rawData.json();
 };
